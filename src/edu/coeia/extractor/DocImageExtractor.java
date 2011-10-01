df3b7d195f0c38fe4220283520160b4837f77289
Wajdy Essam <wajdyessam@ksu.edu.sa>
2011-10-01 14:45:58 +0300

    Define Empty Extraxtors

---
+++ src/edu/coeia/extractor/DocImageExtractor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.coeia.extractor;
+
+/**
+ *
+ * @author wajdyessam
+ */
+
+import java.io.File ;
+
+public class DocImageExtractor implements ImageExtractor{
+    @Override
+    public void extractImages(File file, String distenationFolder) {
+        
+    }
+}
---
+++ src/edu/coeia/indexing/IndexFactory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.coeia.indexing;
+
+/**
+ *
+ * @author wajdyessam
+ */
+
+import java.io.File ;
+import org.apache.lucene.index.IndexWriter;
+
+public class IndexFactory {
+    public IndexFactory(IndexWriter writer, boolean isSupportImageCaching) {
+    }
+    
+    public Index getIndex (File file) {
+        return null;
+    }
+}
