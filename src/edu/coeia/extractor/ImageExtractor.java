13b41e5f6e3fc2b1011f7747f94b4fd7f5df46d7
Wajdy Essam <wajdyessam@ksu.edu.sa>
2011-10-01 14:30:16 +0300

    Add Basic Empty Classes

---
+++ src/edu/coeia/extractor/ImageExtractor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.coeia.extractor;
+
+/**
+ * Image Extractor
+ * Interface for images extractor from different type of files
+ * @author wajdyessam
+ */
+
+import java.io.File ;
+
+public interface ImageExtractor {
+    public void extractImages (File file, String distenationFolder);
+}
---
+++ src/edu/coeia/indexing/Index.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.coeia.indexing;
+
+/**
+ * Abstract Class for defining Index object
+ * @author wajdyessam
+ */
+
+public class Index {
+    
+}
