1f266af11ac9a87c7e71ef6ac3669771447737f3
Wajdy Essam <wajdyessam@ksu.edu.sa>
2012-01-14 08:34:39 +0300

    all new dialog in the old gui layout

---
+++ src/edu/coeia/reports/ReportGenerator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.coeia.reports;
+
+/**
+ *
+ * @author wajdyessam
+ */
+public interface ReportGenerator {
+    public String generateReport();
+}
