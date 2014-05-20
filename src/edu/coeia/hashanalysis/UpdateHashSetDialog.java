53f202700613d9ed9d2ba0362324dddd810b026b
Wajdy Essam <wajdyessam@ksu.edu.sa>
2014-05-20 10:37:28 +0300

    add GPL header to all files

--- src/edu/coeia/hashanalysis/UpdateHashSetDialog.java
+++ src/edu/coeia/hashanalysis/UpdateHashSetDialog.java
@@ -1,12 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * UpdateHashSetDialog.java
+/* 
+ * Copyright (C) 2014 Center of Excellence in Information Assurance
  *
- * Created on Dec 17, 2011, 1:58:17 PM
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package edu.coeia.hashanalysis;
 
